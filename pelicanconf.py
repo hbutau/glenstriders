6b159c54348644b931d8204acc98c5a33b86db22
humphrey <humphrey@buibui.co.zw>
2025-10-24 08:57:15 +0200

    initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,51 @@
+AUTHOR = 'GlenStriders'
+SITENAME = 'GlenStriders | GlenStriders Running Club '
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Africa/Harare'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+# MENUITEMS = [
+#     ("Home", ""),
+#     ("Services", ""),
+#     ("Contact Us", ""),
+#     ]
+
+THEME = "buibui-theme"
+
+CSS_FILE = "styles.css"
+
+# Hero section
+
+HEROTEXT = "We are busy running"
+HERO_CTA_BUTTON_TEXT  = "Join Us On Whatsapp"
+HEROSUBTEXT = "Glen Striders are out chasing kilometers and making memories. Our site is under construction, but we will be back soon!"
